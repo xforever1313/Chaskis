1fea58dbe38026c4fef17b7fec5d7fac7dd63c42
Seth Hendrick <seth@shendrick.net>
2016-04-03 18:24:48 -0400

    Added boost software license.

--- GenericIrcBot/GenericIrcBot/IrcBot.cs
+++ GenericIrcBot/GenericIrcBot/IrcBot.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿
+//          Copyright Seth Hendrick 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+using System;
 
 namespace GenericIrcBot
 {
--- GenericIrcBot/Tests/Test.cs
+++ GenericIrcBot/Tests/Test.cs
@@ -1,6 +1,12 @@
-﻿using NUnit.Framework;
+﻿
+//          Copyright Seth Hendrick 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 using System;
 using GenericIrcBot;
+using NUnit.Framework;
 
 namespace Tests
 {
