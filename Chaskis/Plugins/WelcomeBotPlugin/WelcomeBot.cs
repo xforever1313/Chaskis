48a9148a3bc223d1ae4da63208d8a49adac2e7a0
Seth Hendrick <seth@shendrick.net>
2016-05-04 20:48:17 -0400

    Put WelcomeBot in the Chaskis.Plugins.WelcomeBotPlugin namespace.
    Updated licensing information so it points to the right directory.

--- Chaskis/Plugins/WelcomeBotPlugin/WelcomeBot.cs
+++ Chaskis/Plugins/WelcomeBotPlugin/WelcomeBot.cs
@@ -1,7 +1,7 @@
 ﻿
 //          Copyright Seth Hendrick 2016.
 // Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//    (See accompanying file ../../../LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 //
 
@@ -9,7 +9,7 @@ using System;
 using GenericIrcBot;
 using System.Collections.Generic;
 
-namespace WelcomeBotPlugin
+namespace Chaskis.Plugins.WelcomeBotPlugin
 {
     /// <summary>
     /// This plugin welcomes a user to the channel when he or she joins.
