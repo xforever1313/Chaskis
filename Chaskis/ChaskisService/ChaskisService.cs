3150560c85ad49104093b97e4aa15cee74f2519f
Seth Hendrick <seth@shendrick.net>
2016-05-06 23:10:11 -0400

    Chaskis can now be run as a Windows Service or a Mono Service on linux.
    #progress.

---
+++ Chaskis/ChaskisService/ChaskisService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.ServiceProcess;
+using Chaskis;
+using GenericIrcBot;
+
+namespace ChaskisService
+{
+    public partial class ChaskisService : ServiceBase
+    {
+        // -------- Fields --------
+
+        private IrcBot bot = null;
+
+        // -------- Constructor --------
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ChaskisService()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Called when the service is started.
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart( string[] args )
+        {
+            try
+            {
+                string rootDir = Path.Combine(
+                    Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData ),
+                    "Chaskis"
+                );
+
+                IIrcConfig ircConfig = XmlLoader.ParseIrcConfig( Path.Combine( rootDir, "IrcConfig.xml" ) );
+
+                List<IIrcHandler> configs = new List<IIrcHandler>();
+
+                // Load Plugins.
+                {
+                    IList<AssemblyConfig> pluginList = XmlLoader.ParsePluginConfig( Path.Combine( rootDir, "PluginConfig.xml" ) );
+                    using ( StringWriter strWriter = new StringWriter() )
+                    {
+                        PluginManager manager = new PluginManager( strWriter );
+
+                        foreach ( AssemblyConfig pluginInfo in pluginList )
+                        {
+                            bool success = manager.LoadAssembly( Path.GetFullPath( pluginInfo.AssemblyPath ), pluginInfo.ClassName );
+                            if ( ( success == false ) )
+                            {
+                                this.ExitCode = 1;
+                                this.ChaskisEventLog.WriteEntry(
+                                    "Error loading assembly " + pluginInfo.AssemblyPath + Environment.NewLine + strWriter.ToString(),
+                                    EventLogEntryType.Error
+                                );
+                                Stop();
+                            }
+                        }
+                        configs.AddRange( manager.Handlers );
+                    }
+                }
+
+                // Must always check for pings.
+                configs.Add( new PingHandler() );
+
+                bot = new IrcBot( ircConfig, configs );
+                bot.Start();
+            }
+            catch ( Exception err )
+            {
+                this.ChaskisEventLog.WriteEntry(
+                    "FATAL ERROR:" + Environment.NewLine + err.Message + Environment.NewLine + err.StackTrace,
+                    EventLogEntryType.Error
+                );
+                this.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Called when the service is stopped.
+        /// </summary>
+        protected override void OnStop()
+        {
+            this.bot?.Dispose();
+        }
+
+        /// <summary>
+        /// Called when the system is shutting down.
+        /// </summary>
+        protected override void OnShutdown()
+        {
+            this.bot?.Dispose();
+        }
+    }
+}
---
+++ Chaskis/ChaskisService/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ChaskisService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChaskisProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.ChaskisInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // ChaskisProcessInstaller
+            // 
+            this.ChaskisProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            this.ChaskisProcessInstaller.Password = null;
+            this.ChaskisProcessInstaller.Username = null;
+            // 
+            // ChaskisInstaller
+            // 
+            this.ChaskisInstaller.Description = "A .NET IRC Bot";
+            this.ChaskisInstaller.DisplayName = "Chaskis IRC Bot";
+            this.ChaskisInstaller.ServiceName = "Chaskis";
+            this.ChaskisInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.ChaskisProcessInstaller,
+            this.ChaskisInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller ChaskisProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller ChaskisInstaller;
+    }
+}
