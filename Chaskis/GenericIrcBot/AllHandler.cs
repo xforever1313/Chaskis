5470fdf9aac16860004fbacab70c5abddb3bb30d
Seth Hendrick <seth@shendrick.net>
2016-06-25 15:18:15 -0400

    Added an All handler, which gets fired each time the bot gets an IRC message.

---
+++ Chaskis/GenericIrcBot/AllHandler.cs
@@ -0,0 +1,64 @@
+﻿
+//          Copyright Seth Hendrick 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+using System;
+using SethCS.Exceptions;
+
+namespace GenericIrcBot
+{
+    /// <summary>
+    /// This class will fire for ALL IRC messages and pass in the raw
+    /// IRC message as the message string.
+    /// </summary>
+    public class AllHandler : IIrcHandler
+    {
+        // -------- Constructor --------
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="allAction">The action to take when ANY message appears from IRC (JOIN, PART, PRIVMSG, PING, etc).</param>
+        public AllHandler( Action<IIrcWriter, IrcResponse> allAction )
+        {
+            ArgumentChecker.IsNotNull( allAction, nameof( allAction ) );
+
+            this.AllAction = allAction;
+        }
+
+        // -------- Properties --------
+
+        /// <summary>
+        /// The action to take when ANY message appears from IRC (JOIN, PART, PRIVMSG, PING, etc).
+        /// As far as the passed in IrcResponse to the action goes, the channel and remote user
+        /// will be String.Empty, since this class does no parsing of the IRC message.
+        /// It just grabs the line from the IRC channel and passes it into the AllAction
+        /// with no parsing.  It is up to the AllAction to parse the channel and user
+        /// name if they so desire.
+        /// </summary>
+        public Action<IIrcWriter, IrcResponse> AllAction { get; private set; }
+
+        /// <summary>
+        /// Handles the event.
+        /// </summary>
+        /// <param name="line">The RAW line from IRC to check.</param>
+        /// <param name="ircConfig">The irc config to use when parsing this line.</param>
+        /// <param name="ircWriter">The way to write to the irc channel.</param>
+        public void HandleEvent( string line, IIrcConfig ircConfig, IIrcWriter ircWriter )
+        {
+            ArgumentChecker.StringIsNotNullOrEmpty( line, nameof( line ) );
+            ArgumentChecker.IsNotNull( ircConfig, nameof( ircConfig ) );
+            ArgumentChecker.IsNotNull( ircWriter, nameof( ircWriter ) );
+
+            IrcResponse response = new IrcResponse(
+                string.Empty,
+                string.Empty,
+                line
+            );
+
+            this.AllAction( ircWriter, response );
+        }
+    }
+}
---
+++ Chaskis/Tests/AllHandlerTest.cs
@@ -0,0 +1,186 @@
+﻿
+//          Copyright Seth Hendrick 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+using System;
+using GenericIrcBot;
+using NUnit.Framework;
+using Tests.Mocks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AllHandlerTest
+    {
+        // -------- Fields --------
+
+        /// <summary>
+        /// Unit Under Test.
+        /// </summary>
+        private AllHandler uut;
+
+        /// <summary>
+        /// Irc Config to use.
+        /// </summary>
+        private IrcConfig ircConfig;
+
+        /// <summary>
+        /// The mock IRC connection to use.
+        /// </summary>
+        private MockIrcConnection ircConnection;
+
+        /// <summary>
+        /// The response received from the event handler (if any).
+        /// </summary>
+        private IrcResponse responseReceived;
+
+        /// <summary>
+        /// A user.
+        /// </summary>
+        private const string remoteUser = "remoteuser";
+
+        // -------- Setup / Teardown --------
+
+        [SetUp]
+        public void TestSetup()
+        {
+            this.ircConfig = TestHelpers.GetTestIrcConfig();
+            ircConnection = new MockIrcConnection( this.ircConfig );
+            responseReceived = null;
+            this.uut = new AllHandler( AllFunction );
+        }
+
+        // -------- Tests --------
+
+        /// <summary>
+        /// Ensurs everything that needs to throw argument null
+        /// exceptions does.
+        /// </summary>
+        [Test]
+        public void ArgumentNullTest()
+        {
+            Assert.Throws<ArgumentNullException>( () =>
+                new PartHandler( null )
+            );
+
+            Assert.Throws<ArgumentNullException>( () =>
+                this.uut.HandleEvent( null, new IrcConfig(), ircConnection )
+            );
+            Assert.IsNull( this.responseReceived ); // Ensure handler didn't get called.
+
+            Assert.Throws<ArgumentNullException>( () =>
+                this.uut.HandleEvent( string.Empty, new IrcConfig(), ircConnection )
+            );
+            Assert.IsNull( this.responseReceived ); // Ensure handler didn't get called.
+
+            Assert.Throws<ArgumentNullException>( () =>
+                this.uut.HandleEvent( "hello world", null, ircConnection )
+            );
+            Assert.IsNull( this.responseReceived ); // Ensure handler didn't get called.
+
+            Assert.Throws<ArgumentNullException>( () =>
+                this.uut.HandleEvent( "hello world", new IrcConfig(), null )
+            );
+            Assert.IsNull( this.responseReceived ); // Ensure handler didn't get called.
+        }
+
+        /// <summary>
+        /// Ensures that if the bot parts, the event is fired.
+        /// </summary>
+        [Test]
+        public void BotParts()
+        {
+            string ircString = TestHelpers.ConstructIrcString(
+                ircConfig.Nick,
+                PartHandler.IrcCommand,
+                ircConfig.Channel,
+                string.Empty
+            );
+
+            this.uut.HandleEvent( ircString, this.ircConfig, ircConnection );
+            this.CheckResponse( ircString );
+        }
+
+        /// <summary>
+        /// Ensures that if a PRIMSG appears, the event is fired.
+        /// </summary>
+        [Test]
+        public void MessageCommandAppears()
+        {
+            string ircString = TestHelpers.ConstructIrcString(
+                remoteUser,
+                MessageHandler.IrcCommand,
+                ircConfig.Channel,
+                "A message"
+            );
+
+            this.uut.HandleEvent( ircString, this.ircConfig, ircConnection );
+            this.CheckResponse( ircString );
+        }
+
+        /// <summary>
+        /// Ensures that if a JOIN appears, the event is fired.
+        /// </summary>
+        [Test]
+        public void JoinCommandAppears()
+        {
+            string ircString = TestHelpers.ConstructIrcString(
+                remoteUser,
+                JoinHandler.IrcCommand,
+                ircConfig.Channel,
+                string.Empty
+            );
+
+            this.uut.HandleEvent( ircString, this.ircConfig, ircConnection );
+            this.CheckResponse( ircString );
+        }
+
+        /// <summary>
+        /// Ensures that if a PING appears, the event is fired.
+        /// </summary>
+        [Test]
+        public void PingAppears()
+        {
+            string ircString = TestHelpers.ConstructPingString( "12345" );
+            this.uut.HandleEvent(
+                ircString,
+                this.ircConfig,
+                ircConnection
+            );
+            this.CheckResponse( ircString );
+        }
+
+        // -------- Test Helpers --------
+
+        /// <summary>
+        /// The function that is called 
+        /// </summary>
+        /// <param name="writer">The writer that can be written to.</param>
+        /// <param name="response">The response from the server.</param>
+        private void AllFunction( IIrcWriter writer, IrcResponse response )
+        {
+            Assert.AreSame( this.ircConnection, writer );
+            this.responseReceived = response;
+        }
+
+        /// <summary>
+        /// Ensures the bot responds accordingly.
+        /// </summary>
+        /// <param name="rawIrcString">The raw IRC string we expect.</param>
+        private void CheckResponse( string rawIrcString )
+        {
+            Assert.IsNotNull( this.responseReceived );
+
+            // Ensure its the raw string.
+            Assert.AreEqual( rawIrcString, responseReceived.Message );
+
+            // Channel should be empty, its up to the user to parse it.
+            Assert.AreEqual( string.Empty, responseReceived.Channel );
+
+            // Nick should be empty.  Its up to the user to parse it.
+            Assert.AreEqual( string.Empty, responseReceived.RemoteUser );
+        }
+    }
+}
