e1285c3f4d5cfe0beb2076ca8ce508cb0a8d92ac
Seth Hendrick <seth@shendrick.net>
2016-06-25 22:59:00 -0400

    Added source code link to the ConsoleOut plugin.
    Also added licensing information, since I forgot to do that :|

--- Chaskis/Plugins/ConsoleOut/ConsoleOut.cs
+++ Chaskis/Plugins/ConsoleOut/ConsoleOut.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿
+//          Copyright Seth Hendrick 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+using System;
 using System.Collections.Generic;
 using GenericIrcBot;
 
@@ -37,7 +43,7 @@ namespace Chaskis.Plugins.ConsoleOut
         {
             get
             {
-                return "TBD";
+                return "https://github.com/xforever1313/Chaskis/tree/master/Chaskis/Plugins/ConsoleOut";
             }
         }
 
