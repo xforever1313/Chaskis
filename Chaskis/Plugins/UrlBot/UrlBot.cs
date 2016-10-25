d8cc974618c465323fe9dfb62c6178f72abda81e
Seth Hendrick <seth@shendrick.net>
2016-10-24 22:51:54 -0400

    Began adding URL bot, a bot that gets the description of the URL.
    A lot still needs to be done including:
    - Adding Unit Tests
    - Add HTML parser, instead of regexes everywhere!
    - Add Unit Tests.

---
+++ Chaskis/Plugins/UrlBot/UrlBot.cs
@@ -0,0 +1,116 @@
+﻿//
+//          Copyright Seth Hendrick 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+using System;
+using ChaskisCore;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chaskis.Plugins.UrlBot
+{
+    /// <summary>
+    /// This class takes a URL from the channel, reads the HTML From it, finds
+    /// the description meta tag, and then prints the description to the channel.
+    /// </summary>
+    public class UrlBot : IPlugin
+    {
+        // -------- Fields --------
+
+        /// <summary>
+        /// The handlers for this plugin.
+        /// </summary>
+        private readonly List<IIrcHandler> handlers;
+
+        private readonly UrlReader urlReader;
+
+        // -------- Constructor --------
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public UrlBot()
+        {
+            this.urlReader = new UrlReader();
+            this.handlers = new List<IIrcHandler>();
+        }
+
+        // -------- Properties --------
+
+        /// <summary>
+        /// The location of the source code.
+        /// </summary>
+        public string SourceCodeLocation
+        {
+            get
+            {
+                return "https://github.com/xforever1313/Chaskis/tree/master/Chaskis/Plugins/UrlBot";
+            }
+        }
+
+        // -------- Functions --------
+
+        /// <summary>
+        /// Initializes the plugin.  This includes loading any configuration files,
+        /// starting services, etc.  Allowed to throw Exceptions.
+        ///
+        /// This function should be used to validates that the environment is good for the plugin.
+        /// For example, it has all dependencies installed, config files are in the correct spot, etc.
+        /// It should also load GetHandlers() with the handlers.
+        /// </summary>
+        /// <param name="pluginPath">
+        /// The absolute path to the plugin, including the file name.  To just get
+        /// the path to the plugin, call Path.GetDirectoryName on this argument.
+        /// </param>
+        /// <param name="ircConfig">The IRC config we are using.</param>
+        public void Init( string pluginPath, IIrcConfig ircConfig )
+        {
+            MessageHandler handler = new MessageHandler(
+                ".+",
+                this.HandleMessage
+            );
+
+            this.handlers.Add( handler );
+        }
+
+        /// <summary>
+        /// Gets the handlers that should be added to the main bot.
+        /// </summary>
+        /// <returns>The list of handlers to awtch.</returns>
+        public IList<IIrcHandler> GetHandlers()
+        {
+            return this.handlers.AsReadOnly();
+        }
+
+        /// <summary>
+        /// Tears down the plugin.
+        /// </summary>
+        public void Teardown()
+        {
+        }
+
+        /// <summary>
+        /// Handles a message from the channel.
+        /// </summary>
+        /// <param name="writer">The IRC Writer to write to.</param>
+        /// <param name="response">The response from the channel.</param>
+        private async void HandleMessage( IIrcWriter writer, IrcResponse response )
+        {
+            string url;
+            if( UrlReader.TryParseUrl( response.Message, out url ) )
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.AppendFormat( "@{0}: Here's a description of that URL: ", response.RemoteUser );
+                builder.Append( await this.urlReader.GetDescription( url ) );
+
+                writer.SendMessageToUser(
+                    builder.ToString(),
+                    response.Channel
+                );
+            }
+        }
+    }
+}
+
---
+++ Chaskis/Plugins/UrlBot/UrlReader.cs
@@ -0,0 +1,112 @@
+﻿//
+//          Copyright Seth Hendrick 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Chaskis.Plugins.UrlBot
+{
+    /// <summary>
+    /// This class is the class that reads the URL,
+    /// parses the html for the description meta tag,
+    /// and then returns the description as a string.
+    /// 
+    /// Maybe someday we'll add cacheing too...
+    /// </summary>
+    public class UrlReader
+    {
+        // -------- Fields --------
+
+        /// <summary>
+        /// Regex for a URL.
+        /// Taken from (and slightly tweaked) from: https://weblogs.asp.net/farazshahkhan/regex-to-find-url-within-text-and-make-them-as-link
+        /// </summary>
+        public const string UrlRegex =
+            @"(?<url>(http://|https://|ftp://|file:///)([\w+?\.\w+])+([a-zA-Z0-9\~\!\@\#\$\%\^\&amp;\*\(\)_\-\=\+\\\/\?\.\:\;\'\,]*)?)";
+
+        private static readonly Regex urlRegex = new Regex( UrlRegex, RegexOptions.IgnoreCase | RegexOptions.Compiled );
+
+        /// <summary>
+        /// Regex for finding the description meta tag.
+        /// Yeah, Yeah, using a regex for HTML is bad, blah, blah blah.
+        /// At some point we should use an HTML parser, but we'll see what happens...
+        /// The less depencencies the better.
+        /// </summary>
+        public const string MetaDescriptionRegex = 
+            @"\<\s*meta\s+name\s*=\s*""description""\s+content\s*=\s*""(?<description>[^\<\>]+)""\s*/?>";
+
+        private static readonly Regex metaRegex = new Regex( MetaDescriptionRegex, RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Singleline );
+
+        // -------- Constructor --------
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public UrlReader()
+        {
+        }
+
+        // -------- Properties --------
+
+        // -------- Functions --------
+
+        /// <summary>
+        /// Tries to find the URL from the given string.
+        /// </summary>
+        /// <param name="stringToParse">The string to parse</param>
+        /// <param name="url">The found URL as a string.</param>
+        /// <returns><c>true</c>, if we found a URL, <c>false</c> otherwise.</returns>
+        public static bool TryParseUrl( string stringToParse, out string url )
+        {
+            Match match = urlRegex.Match( stringToParse );
+            if( match.Success == false )
+            {
+                url = string.Empty;
+                return false;
+            }
+
+            url = match.Groups["url"].Value;
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the description of the given URL in a background thread.
+        /// </summary>
+        /// <param name="url">URL to grab.</param>
+        /// <returns>The description from the website's meta tag.</returns>
+        public Task<string> GetDescription( string url )
+        {
+            return Task<string>.Run(
+                delegate()
+                {
+                    try
+                    {
+                        string webResponse;
+                        using( WebClient client = new WebClient() )
+                        {
+                            webResponse = client.DownloadString( url );
+                        }
+
+                        Match match = metaRegex.Match( webResponse );
+                        if ( match.Success )
+                        {
+                            return match.Groups["description"].Value;
+                        }
+
+                        return "Can not find description tag :(";
+                    }
+                    catch( Exception e )
+                    {
+                        return "Error getting site information: " + e.Message;
+                    }
+                }
+            );
+        }
+    }
+}
+
