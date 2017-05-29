89de28be95647c3d85e359671448eae8229223aa
Seth Hendrick <seth@shendrick.net>
2017-05-29 18:19:08 -0400

    Added RssBot config C# class and Unit Tests.

---
+++ Chaskis/Plugins/RssBot/RssBotConfig.cs
@@ -0,0 +1,152 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SethCS.Exceptions;
+
+namespace Chaskis.Plugins.RssBot
+{
+    public class Feed
+    {
+        // ---------------- Constructor ----------------
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Feed()
+        {
+            this.Url = string.Empty;
+            this.RefreshInterval = TimeSpan.Zero;
+        }
+
+        // ---------------- Properties ----------------
+
+        /// <summary>
+        /// The URL to pull.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// How often to pull the feed to check for updates.
+        /// </summary>
+        public TimeSpan RefreshInterval { get; set; }
+
+        // ---------------- Functions ----------------
+
+        /// <summary>
+        /// Ensures this feed object was created correctly.
+        /// </summary>
+        /// <param name="errorString">What is wrong with this object in string form.  Empty if there is nothing.</param>
+        /// <returns>True if this feed is a valid object, else false.</returns>
+        public bool TryValidate( out string errorString )
+        {
+            bool success = true;
+            StringBuilder errorBuilder = new StringBuilder();
+            errorBuilder.AppendLine( "The following is wrong with a Feed:" );
+
+            if( string.IsNullOrEmpty( this.Url ) || string.IsNullOrWhiteSpace( this.Url ) )
+            {
+                success = false;
+                errorBuilder.AppendLine( "\t-URL can not be empty, null, or whitespace." );
+            }
+            if( this.RefreshInterval <= TimeSpan.Zero )
+            {
+                success = false;
+                errorBuilder.AppendLine( "\t-Refresh interval can not be negative or zero." );
+            }
+
+            if( success )
+            {
+                errorString = string.Empty;
+            }
+            else
+            {
+                errorString = errorBuilder.ToString();
+            }
+
+            return success;
+        }
+
+        /// <summary>
+        /// Creates a deep-copy of this object.
+        /// </summary>
+        public Feed Clone()
+        {
+            return (Feed)this.MemberwiseClone();
+        }
+    }
+
+    public class RssBotConfig
+    {
+        // ---------------- Fields ----------------
+
+        private List<Feed> feeds;
+
+        // ---------------- Constructor ----------------
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public RssBotConfig()
+        {
+            this.feeds = new List<Feed>();
+            this.Feeds = feeds.AsReadOnly();
+        }
+
+        // ---------------- Properties ----------------
+
+        /// <summary>
+        /// Read-only list of feeds.
+        /// </summary>
+        public IReadOnlyList<Feed> Feeds { get; private set; }
+
+        // ---------------- Functions ----------------
+
+        /// <summary>
+        /// Adds a cloned version of the passed-in feed
+        /// to the feed list.
+        /// </summary>
+        /// <param name="feed">The feed to add.</param>
+        public void AddFeed( Feed feed )
+        {
+            this.feeds.Add( feed.Clone() );
+        }
+
+        /// <summary>
+        /// Ensures all feeds are valid.
+        /// </summary>
+        /// <exception cref="ValidationException">If a feed is invalid.</exception>
+        public void Validate()
+        {
+            bool success = true;
+            StringBuilder errorBuilder = new StringBuilder();
+
+            if( this.Feeds.Count == 0 )
+            {
+                success = false;
+                errorBuilder.AppendLine( "No feeds have been added!" );
+            }
+
+            foreach( Feed feed in this.Feeds )
+            {
+                string errorString;
+                if( feed.TryValidate( out errorString ) == false )
+                {
+                    success = false;
+                    errorBuilder.AppendLine( errorString );
+                }
+            }
+
+            if( success == false )
+            {
+                throw new ValidationException( errorBuilder.ToString() );
+            }
+        }
+    }
+}
---
+++ Chaskis/Tests/Plugins/RssBot/RssBotConfigTest.cs
@@ -0,0 +1,131 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using Chaskis.Plugins.RssBot;
+using NUnit.Framework;
+using SethCS.Exceptions;
+
+namespace Tests.Plugins.RssBot
+{
+    [TestFixture]
+    public class RssBotConfigTest
+    {
+        // ---------------- Setup / Teardown ----------------
+
+        // ---------------- Tests ----------------
+
+        /// <summary>
+        /// Ensures the Feed's Clone() function works as expected.
+        /// </summary>
+        [Test]
+        public void FeedCloneTest()
+        {
+            Feed feed1 = new Feed();
+            feed1.Url = RssBotTestHelpers.TestUrl1;
+            feed1.RefreshInterval = TimeSpan.MaxValue;
+
+            Feed clonedFeed = feed1.Clone();
+
+            Assert.AreNotSame( feed1, clonedFeed );
+            Assert.AreEqual( feed1.Url, clonedFeed.Url );
+            Assert.AreEqual( feed1.RefreshInterval, clonedFeed.RefreshInterval );
+        }
+
+        /// <summary>
+        /// Ensures our TryValidate function on Feed works as expected.
+        /// </summary>
+        [Test]
+        public void FeedValidateTest()
+        {
+            Feed feed = new Feed();
+            feed.Url = RssBotTestHelpers.TestUrl1;
+            feed.RefreshInterval = TimeSpan.Zero + TimeSpan.FromTicks( 1 ); // Positive is good.
+
+            // Good case.
+            {
+                string errorString;
+                Assert.IsTrue( feed.TryValidate( out errorString ) );
+                Assert.IsEmpty( errorString );
+            }
+
+            // Empty URL.
+            {
+                feed.Url = string.Empty;
+                string errorString;
+                Assert.IsFalse( feed.TryValidate( out errorString ) );
+                Assert.IsNotEmpty( errorString );
+            }
+
+            // Null URL.
+            {
+                feed.Url = null;
+                string errorString;
+                Assert.IsFalse( feed.TryValidate( out errorString ) );
+                Assert.IsNotEmpty( errorString );
+            }
+
+            // Whitespace URL.
+            {
+                feed.Url = "   ";
+                string errorString;
+                Assert.IsFalse( feed.TryValidate( out errorString ) );
+                Assert.IsNotEmpty( errorString );
+            }
+
+            feed.Url = RssBotTestHelpers.TestUrl1;
+
+            // Zero timespan.
+            {
+                feed.RefreshInterval = TimeSpan.Zero;
+                string errorString;
+                Assert.IsFalse( feed.TryValidate( out errorString ) );
+                Assert.IsNotEmpty( errorString );
+            }
+
+            // Negative Timespan
+            {
+                feed.RefreshInterval = TimeSpan.MinValue;
+                string errorString;
+                Assert.IsFalse( feed.TryValidate( out errorString ) );
+                Assert.IsNotEmpty( errorString );
+            }
+        }
+
+        /// <summary>
+        /// Ensures RssConfig's validation function works as expected.
+        /// </summary>
+        [Test]
+        public void RssConfigValidateTest()
+        {
+            Feed feed1 = new Feed();
+            feed1.Url = RssBotTestHelpers.TestUrl1;
+            feed1.RefreshInterval = TimeSpan.Zero + TimeSpan.FromTicks( 1 ); // Positive is good.
+
+            Feed feed2 = new Feed();
+            feed2.Url = RssBotTestHelpers.TestUrl2;
+            feed2.RefreshInterval = TimeSpan.MaxValue;
+
+            RssBotConfig config = new RssBotConfig();
+
+            // Empty list should fail.
+            Assert.Throws<ValidationException>( () => config.Validate() );
+
+            config.AddFeed( feed1 );
+            config.AddFeed( feed2 );
+            Assert.DoesNotThrow( () => config.Validate() );
+
+            // Ensure things get cloned by changing feed 1, and validation should still pass.
+            feed1.Url = null;
+            Assert.DoesNotThrow( () => config.Validate() );
+
+            // Now add bad feed 1, and we should fail validation.
+            config.AddFeed( feed1 );
+            Assert.Throws<ValidationException>( () => config.Validate() );
+        }
+    }
+}
