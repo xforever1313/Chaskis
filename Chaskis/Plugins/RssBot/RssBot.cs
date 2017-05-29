b05e3575e66a146d94ef57960d971ee376914eab
Seth Hendrick <seth@shendrick.net>
2017-05-29 14:33:16 -0400

    Added stub for RssBot.
    - Added Sample XML config file and Schema
    - Added stub plugin.

---
+++ Chaskis/Plugins/RssBot/RssBot.cs
@@ -0,0 +1,132 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChaskisCore;
+
+namespace Chaskis.Plugins.RssBot
+{
+    /// <summary>
+    /// RSS Bot pulls from RSS feeds and posts any updates
+    /// to the IRC channel.
+    /// </summary>
+    [ChaskisPlugin( "rssbot" )]
+    public class RssBot : IPlugin
+    {
+        // ---------------- Fields ----------------
+
+        public const string VersionStr = "0.1.0";
+
+        /// <summary>
+        /// The handlers for this plugin.
+        /// </summary>
+        private readonly List<IIrcHandler> handlers;
+
+        private IIrcConfig ircConfig;
+
+        // ---------------- Constructor ----------------
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public RssBot()
+        {
+            this.handlers = new List<IIrcHandler>();
+        }
+
+        // ---------------- Properties ----------------
+
+        /// <summary>
+        /// The location of the source code.
+        /// </summary>
+        public string SourceCodeLocation
+        {
+            get
+            {
+                return "https://github.com/xforever1313/Chaskis/tree/master/Chaskis/Plugins/RssBot";
+            }
+        }
+
+        /// <summary>
+        /// This plugin's version.
+        /// </summary>
+        public string Version
+        {
+            get
+            {
+                return VersionStr;
+            }
+        }
+
+        /// <summary>
+        /// About this plugin.
+        /// </summary>
+        public string About
+        {
+            get
+            {
+                return "I post updates to RSS and ATOM feeds when they get updated.";
+            }
+        }
+
+        // ---------------- Functions ----------------
+
+        /// <summary>
+        /// Initializes the plugin.
+        /// </summary>
+        /// <param name="pluginPath">The absolute path to the plugin dll.</param>
+        /// <param name="ircConfig">The IRC config we are using.</param>
+        public void Init( string pluginPath, IIrcConfig ircConfig )
+        {
+            string configPath = Path.Combine(
+                Path.GetDirectoryName( pluginPath ),
+                "RssBotConfig.xml"
+            );
+
+            if( File.Exists( configPath ) == false )
+            {
+                throw new FileNotFoundException(
+                    "Can not open " + configPath
+                );
+            }
+
+            this.ircConfig = ircConfig;
+        }
+
+        /// <summary>
+        /// Handles the help message.
+        /// </summary>
+        public void HandleHelp( IIrcWriter writer, IrcResponse response, string[] args )
+        {
+            writer.SendMessageToUser(
+                this.About + "  Don't like one of the feeds I post?  Yell at my admin!",
+                response.Channel
+            );
+        }
+
+        /// <summary>
+        /// Gets the handlers that should be added to the main bot.
+        /// </summary>
+        /// <returns>The list of handlers to awtch.</returns>
+        public IList<IIrcHandler> GetHandlers()
+        {
+            return this.handlers.AsReadOnly();
+        }
+
+        /// <summary>
+        /// Tears down the plugin.
+        /// </summary>
+        public void Dispose()
+        {
+        }
+    }
+}
