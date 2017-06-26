4610c61077c49b8f5b4b6ed913847ad7234506e1
Seth Hendrick <seth@shendrick.net>
2017-06-25 23:13:34 -0400

    Issue: #5 Added Cli Installer.
    This takes four arguments:
     - Solution Directory
     - The root where to put the Chaskis folder for install
     - The path to the WIX XML folder.
     - Which target (Debug or Release) we are doing.
    
    What this program does is parse the WIX folder and install everything in the specified root directory.  This will be useful during PKGBUILD so we don't need to copy everything manually, just run this program with the correct arguments.  It also means we only need one XML File for installing.
    
    Let's hope this crazy thing works...

---
+++ Chaskis/ChaskisCliInstaller/Installer.cs
@@ -0,0 +1,402 @@
+﻿//
+//          Copyright Seth Hendrick 2016-2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace Chaskis.ChaskisCliInstaller
+{
+    /// <summary>
+    /// The class that puts the files where they are supposed to go.
+    /// </summary>
+    public class Installer
+    {
+        // ---------------- Fields ----------------
+
+        private Dictionary<string, DirectoryInfo> dirInfo;
+
+        private const string parentDir = "INSTALLFOLDER";
+
+        private readonly string wixArgName;
+
+        // ---------------- Constructor ----------------
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="rootDir">
+        /// The install root.  The Chaskis directory will be placed in here.
+        /// </param>
+        /// <param name="wixFile">The WIX Xml file we'll use to parse and figure out where to install things.</param>
+        public Installer( string slnDir, string rootDir, string wixFile, string target )
+        {
+            this.wixArgName = nameof( wixFile );
+
+            if( Directory.Exists( slnDir ) == false )
+            {
+                throw new DirectoryNotFoundException( "Directory " + slnDir + " does not exist." );
+            }
+            else if( File.Exists( Path.Combine( slnDir, "Chaskis.sln" ) ) == false )
+            {
+                throw new DirectoryNotFoundException( "Directory " + slnDir + " does not contain Chaskis.sln." );
+            }
+
+            if( Directory.Exists( rootDir ) == false )
+            {
+                throw new DirectoryNotFoundException( "Directory " + rootDir + " does not exist." );
+            }
+
+            if( File.Exists( wixFile ) == false )
+            {
+                throw new FileNotFoundException( "File " + wixFile + " does not exist." );
+            }
+
+            this.SlnDir = Path.GetFullPath( slnDir );
+            this.RootDir = Path.GetFullPath( rootDir );
+            this.WixFile = Path.GetFullPath( wixFile );
+
+            if( ( target.ToLower() == "debug" ) )
+            {
+                this.Target = "Debug";
+            }
+            else if( target.ToLower() == "release" )
+            {
+                this.Target = "Release";
+            }
+            else
+            {
+                throw new ArgumentException( "Target must be debug or release.  Got " + target, nameof( target ) );
+            }
+
+            this.dirInfo = new Dictionary<string, DirectoryInfo>();
+        }
+
+        // ---------------- Properties ----------------
+
+        /// <summary>
+        /// The install root.  The Chaskis directory will be placed in here.
+        /// </summary>
+        public string RootDir { get; private set; }
+
+        /// <summary>
+        /// The WIX Xml file we'll use to parse and figure out where to install things.
+        /// </summary>
+        public string WixFile { get; private set; }
+
+        /// <summary>
+        /// Our target (Debug or Release).
+        /// </summary>
+        public string Target { get; private set; }
+
+        /// <summary>
+        /// Our solution directory.
+        /// </summary>
+        public string SlnDir { get; private set; }
+
+        // ---------------- Functions ----------------
+
+        /// <summary>
+        /// Starts the install.
+        /// </summary>
+        public void Start()
+        {
+            this.Status(
+                string.Format(
+                    "Installing Chaskis to {0} using XML file {1} and target {2}",
+                    this.RootDir,
+                    this.WixFile,
+                    this.Target
+                )
+            );
+
+            this.ParseWixFile();
+        }
+
+        /// <summary>
+        /// Parses the WIX file so we know which files to grab and where to put them.
+        /// </summary>
+        private void ParseWixFile()
+        {
+            XmlDocument wixDoc = new XmlDocument();
+            wixDoc.Load( this.WixFile );
+
+            XmlNode rootNode = wixDoc.DocumentElement;
+            if( rootNode.Name != "Wix" )
+            {
+                throw new ArgumentException( "Passed in Wix XML file does not have a 'Wix' root element", this.wixArgName );
+            }
+
+            foreach( XmlNode childNode in rootNode.ChildNodes )
+            {
+                if( childNode.Name == "Fragment" )
+                {
+                    foreach( XmlNode dirNode in childNode.ChildNodes )
+                    {
+                        if( dirNode.Name == "Directory" )
+                        {
+                            this.ParseDirectories( dirNode, null );
+                        }
+                    }
+                }
+            }
+
+            IList<Tuple<string, string>> files = null;
+            foreach( XmlNode childNode in rootNode.ChildNodes )
+            {
+                if( childNode.Name == "Fragment" )
+                {
+                    foreach( XmlAttribute attr in childNode.Attributes )
+                    {
+                        if( ( attr.Name == "Id" ) && ( attr.Value == "Components" ) )
+                        {
+                            files = this.ParseComponents( childNode );
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if( files == null )
+            {
+                throw new ArgumentException( "No ComponentGroup fragments found...", this.wixArgName );
+            }
+
+            foreach( Tuple<string, string> file in files )
+            {
+                string destDir = Path.Combine( this.RootDir, file.Item2 );
+                this.Status( "Copy " + file.Item1 + "\tto\t" + destDir );
+                if( Directory.Exists( Path.GetDirectoryName( destDir ) ) == false )
+                {
+                    Directory.CreateDirectory( Path.GetDirectoryName( destDir ) );
+                }
+                File.Copy( file.Item1, destDir, true );
+            }
+        }
+
+        private void ParseDirectories( XmlNode installFolder, string parent )
+        {
+            DirectoryInfo parentInfo = new DirectoryInfo();
+            parentInfo.Id = installFolder.Attributes["Id"].Value;
+            if( installFolder.Attributes["Name"] != null )
+            {
+                parentInfo.Name = installFolder.Attributes["Name"].Value;
+            }
+            else
+            {
+                parentInfo.Name = parentInfo.Id;
+            }
+            parentInfo.Parent = parent;
+            this.dirInfo.Add( parentInfo.Id, parentInfo );
+
+            foreach( XmlNode dirNode in installFolder.ChildNodes )
+            {
+                if( dirNode.Name == "Directory" )
+                {
+                    this.ParseDirectories( dirNode, parentInfo.Id );
+                }
+            }
+        }
+
+        private IList<Tuple<string, string>> ParseComponents( XmlNode fragment )
+        {
+            List<Tuple<string, string>> files = new List<Tuple<string, string>>();
+
+            foreach( XmlNode componentGroup in fragment.ChildNodes )
+            {
+                if( componentGroup.Name == "ComponentGroup" )
+                {
+                    foreach( XmlNode component in componentGroup.ChildNodes )
+                    {
+                        if( component.Name == "Component" )
+                        {
+                            XmlAttribute dirAttr = component.Attributes["Directory"];
+                            if( dirAttr == null )
+                            {
+                                throw new ArgumentException(
+                                    "Wix file has a component but no directory attached on component " + component.Name,
+                                    this.wixArgName
+                                );
+                            }
+                            string directory = dirAttr.Value;
+                            if( this.dirInfo.ContainsKey( directory ) == false )
+                            {
+                                throw new ArgumentException(
+                                    "Wix file has a missing directory reference. " + directory + " not defined.",
+                                    this.wixArgName
+                                );
+                            }
+
+                            foreach( XmlNode file in component.ChildNodes )
+                            {
+                                if( file.Name == "File" )
+                                {
+                                    XmlAttribute source = file.Attributes["Source"];
+                                    if( source == null )
+                                    {
+                                        throw new ArgumentException(
+                                            "File is missing a source. File: " + file.Name,
+                                            this.wixArgName
+                                        );
+                                    }
+
+                                    string originalDir = this.GetComponentPath( source.Value );
+
+                                    string relPath;
+                                    TryParseVar( source.Value, out relPath );
+                                    string dest =
+                                        Path.Combine(
+                                            this.GetPath( this.dirInfo[directory] ),
+                                            Path.GetFileName( relPath )
+                                        );
+
+                                    Tuple<string, string> f = new Tuple<string, string>(
+                                        originalDir,
+                                        dest
+                                    );
+                                    files.Add( f );
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return files;
+        }
+
+        /// <summary>
+        /// Gets the component's (the thing we want to copy)'s full path.
+        /// </summary>
+        /// <param name="source">From the XML File.</param>
+        /// <returns>The ABS path of the thing we wish to copy.</returns>
+        private string GetComponentPath( string source )
+        {
+            source = this.GetComponentAbsPath( source );
+
+            if( Environment.OSVersion.Platform == PlatformID.Unix || Environment.OSVersion.Platform == PlatformID.MacOSX )
+            {
+                source = source.Replace( "\\", "/" );
+            }
+
+            return Path.Combine(
+                Path.GetDirectoryName( this.WixFile ),
+                source
+            );
+        }
+
+        private bool TryParseVar( string source, out string fileName )
+        {
+            Match match = Regex.Match( source, @"\$\(var\.(?<plugin>\S+)\.(?<target>TargetPath|TargetDir)\)(?<file>\S+)?" );
+            if( match.Success )
+            {
+                string pluginName = match.Groups["plugin"].Value;
+                if( match.Groups["target"].Value == "TargetPath" )
+                {
+                    if( pluginName == "Chaskis" )
+                    {
+                        fileName = Path.Combine( this.SlnDir, "Chaskis", "bin", this.Target, "Chaskis.exe" );
+                    }
+                    else if( pluginName == "ChaskisCore" )
+                    {
+                        fileName = Path.Combine( this.SlnDir, "ChaskisCore", "bin", this.Target, "ChaskisCore.dll" );
+                    }
+                    else if( pluginName == "ChaskisService" )
+                    {
+                        fileName = Path.Combine( this.SlnDir, "ChaskisService", "bin", this.Target, "ChaskisService.exe" );
+                    }
+                    else
+                    {
+                        fileName = Path.Combine( this.SlnDir, "Plugins", pluginName, "bin", this.Target, pluginName + ".dll" );
+                    }
+                }
+                else
+                {
+                    string f = match.Groups["file"].Value;
+                    fileName = Path.Combine( this.SlnDir, "Plugins", pluginName, "bin", this.Target, f );
+                }
+            }
+            else
+            {
+                fileName = source;
+            }
+
+            return match.Success;
+        }
+
+        /// <summary>
+        /// Gets the path of the thing we want to copy.
+        /// </summary>
+        /// <param name="source">The path from the XML file.</param>
+        private string GetComponentAbsPath( string source )
+        {
+            Match match = Regex.Match( source, @"\$\(var\.(?<plugin>\S+)\.(?<target>TargetPath|TargetDir)\)(?<file>\S+)?" );
+
+            if( TryParseVar( source, out source ) == false )
+            {
+                source = Path.Combine(
+                    // Paths are relative to the wix file.
+                    Path.GetDirectoryName( this.WixFile ),
+                    source
+                );
+            }
+
+            return source;
+        }
+
+        /// <summary>
+        /// Climbs the tree of directory info and gets the relative path.
+        /// </summary>
+        private string GetPath( DirectoryInfo info )
+        {
+            if( info.Id == parentDir )
+            {
+                return Path.Combine( info.Name );
+            }
+
+            List<string> parents = new List<string>();
+            do
+            {
+                parents.Add( info.Name );
+                info = this.dirInfo[info.Parent ?? parentDir];
+            }
+            while( info.Id != parentDir );
+            parents.Add( info.Name );
+
+            parents.Reverse();
+            return Path.Combine( parents.ToArray() );
+        }
+
+        /// <summary>
+        /// Prints status to console out.
+        /// </summary>
+        /// <param name="str"></param>
+        private void Status( string str )
+        {
+            Console.WriteLine( str );
+        }
+
+        // ---------------- Helper Classes ----------------
+
+        private class DirectoryInfo
+        {
+            public DirectoryInfo()
+            {
+            }
+
+            // ---------------- Properties ----------------
+
+            public string Name { get; set; }
+
+            public string Id { get; set; }
+
+            public string Parent { get; set; }
+        }
+    }
+}
---
+++ Chaskis/ChaskisCliInstaller/Program.cs
@@ -0,0 +1,59 @@
+﻿//
+//          Copyright Seth Hendrick 2016-2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaskis.ChaskisCliInstaller
+{
+    /// <summary>
+    /// This program installs chaskis.
+    /// </summary>
+    class Program
+    {
+        static int Main( string[] args )
+        {
+            if( args.Length != 4 )
+            {
+                PrintUsage();
+                return 0;
+            }
+
+            try
+            {
+                Installer installer = new Installer( args[0], args[1], args[2], args[3] );
+                installer.Start();
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine( e.ToString() );
+                return e.GetHashCode();
+            }
+
+            return 0;
+        }
+
+        static void PrintUsage()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine( "This CLI program is used to install Chaskis." );
+            builder.AppendLine();
+            builder.AppendLine( "Usage: ChaskisCliInstaller.exe slnDir rootDir wixXmlFile debug|release" );
+            builder.AppendLine();
+            builder.AppendLine( "slnDir - Where Chaskis.sln lives" );
+            builder.AppendLine( "rootDir - Where the install root will be (e.g. /usr/lib/).  The Chaskis folder gets created in here." );
+            builder.AppendLine( "wixXmlFile - The Wix XML file to use." );
+            builder.AppendLine( "debug|release - Which target we are using." );
+
+            Console.WriteLine( builder.ToString() );
+        }
+    }
+}
