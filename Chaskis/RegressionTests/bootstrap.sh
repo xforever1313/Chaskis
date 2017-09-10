aec8315ac95585cd00c5efdf523a094a252474db
Seth Hendrick <seth@shendrick.net>
2017-09-10 17:53:23 -0400

    Added Linux bootstrapper for FitNesse

---
+++ Chaskis/RegressionTests/bootstrap.sh
@@ -0,0 +1,5 @@
+# Use this to bootstrap your FitNesse directory.
+
+mkdir -p runner
+cp ../packages/NetRunner.1.0.11/tools/net45/NetRunner.Executable.exe ./runner/
+cp ../packages/NetRunner.1.0.11/lib/portable-net4+sl4+wp7+win8/* ./runner/
