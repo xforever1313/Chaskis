dfd732af147371676d14748104ddf39bf2a2997f
Seth Hendrick <seth@shendrick.net>
2017-09-10 16:17:29 -0400

    Added FitNesse.
    Stipped out default stuff.

---
+++ Chaskis/RegressionTests/TestFixtures/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿//
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetRunner.ExternalLibrary;
+
+namespace RegressionTests
+{
+    /// <summary>
+    /// This test just ensures our FitNesse environment is working correctly.
+    /// </summary>
+    public class HelloWorld : BaseTestContainer
+    {
+        public bool IsHelloWorld( string inputLine )
+        {
+            return string.Equals( inputLine, "Hello World" );
+        }
+    }
+}
