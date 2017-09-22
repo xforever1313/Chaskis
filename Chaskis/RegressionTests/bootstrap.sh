b8342279af7bb245fd148cf153ea27aecc59a58f
Seth Hendrick <seth@shendrick.net>
2017-09-22 17:22:44 -0400

    Added MathBot Regression Tests.

--- Chaskis/RegressionTests/bootstrap.sh
+++ Chaskis/RegressionTests/bootstrap.sh
@@ -2,4 +2,6 @@
 
 mkdir -p runner
 cp ../packages/NetRunner.1.0.11/tools/net45/NetRunner.Executable.exe ./runner/
+chmod 700 ./runner/NetRunner.Executable.exe
+
 cp ../packages/NetRunner.1.0.11/lib/portable-net4+sl4+wp7+win8/* ./runner/
