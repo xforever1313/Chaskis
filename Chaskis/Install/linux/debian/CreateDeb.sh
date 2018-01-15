f43616a9c4fc83c98f66524544780de5e6972ed0
Seth Hendrick <seth@shendrick.net>
2018-01-14 23:10:00 -0500

    Added the ability to create a .deb file.

---
+++ Chaskis/Install/linux/debian/CreateDeb.sh
@@ -0,0 +1,41 @@
+###
+# Compile
+###
+
+chaskisroot=../../../
+
+(cd $chaskisroot && nuget restore ./Chaskis.sln)
+(cd $chaskisroot && exec xbuild /p:Configuration=Release ./Chaskis.sln)
+
+###
+# Copy files
+###
+
+pkgdir=`pwd`/chaskis
+debiandir=$pkgdir/DEBIAN
+bindir=$pkgdir/usr/bin
+libdir=$pkgdir/usr/lib
+
+mkdir -p $debiandir
+mkdir -p $pkgdir
+mkdir -p $bindir
+mkdir -p $libdir
+
+# Systemd service
+mkdir -p $libdir/systemd/user/
+cp ../systemd/chaskis.service $libdir/systemd/user/chaskis.service
+
+# Binary
+cp ../bin/chaskis $bindir/chaskis
+
+# Everything else
+(cd $chaskisroot &&
+mono ./Install/ChaskisCliInstaller/bin/Release/ChaskisCliInstaller.exe ./ $libdir ./Install/windows/Product.wxs Release)
+
+# Control
+cp ./control $debiandir/control
+
+###
+# Create Package
+###
+dpkg-deb --build chaskis
