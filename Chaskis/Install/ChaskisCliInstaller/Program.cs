e9d1728d170d8d2a5b083e6ba263689a870757a1
Seth Hendrick <seth@shendrick.net>
2018-06-24 18:25:04 -0400

    Added back in CLI Installer project.
    Updated so it can handle the new file structure.

--- Chaskis/Install/ChaskisCliInstaller/Program.cs
+++ Chaskis/Install/ChaskisCliInstaller/Program.cs
@@ -20,7 +20,7 @@ namespace Chaskis.ChaskisCliInstaller
     {
         static int Main( string[] args )
         {
-            if( args.Length != 4 )
+            if( args.Length != 6 )
             {
                 PrintUsage();
                 return 0;
@@ -28,7 +28,7 @@ namespace Chaskis.ChaskisCliInstaller
 
             try
             {
-                Installer installer = new Installer( args[0], args[1], args[2], args[3] );
+                Installer installer = new Installer( args[0], args[1], args[2], args[3], args[4], args[5] );
                 installer.Start();
             }
             catch( Exception e )
@@ -46,12 +46,14 @@ namespace Chaskis.ChaskisCliInstaller
 
             builder.AppendLine( "This CLI program is used to install Chaskis." );
             builder.AppendLine();
-            builder.AppendLine( "Usage: ChaskisCliInstaller.exe slnDir rootDir wixXmlFile debug|release" );
+            builder.AppendLine( "Usage: ChaskisCliInstaller.exe slnDir rootDir wixXmlFile debug|release exeruntime pluginruntime" );
             builder.AppendLine();
             builder.AppendLine( "slnDir - Where Chaskis.sln lives" );
             builder.AppendLine( "rootDir - Where the install root will be (e.g. /usr/lib/).  The Chaskis folder gets created in here." );
             builder.AppendLine( "wixXmlFile - The Wix XML file to use." );
             builder.AppendLine( "debug|release - Which target we are using." );
+            builder.AppendLine( "exeruntime - Which dotnet runtime we are using for executables." );
+            builder.AppendLine( "pluginruntime - Which dotnet runtime or standard we are using for plugins" );
 
             Console.WriteLine( builder.ToString() );
         }
