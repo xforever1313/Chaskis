df2b2d429f98380a05edb1b4bf1cc570c0c5beec
Seth Hendrick <seth@shendrick.net>
2018-07-06 23:21:41 -0400

    UnitTests are able to be run with OpenCover... that is if OpenCover ever gets around to working with portable PDBs (we may need to enable those on each project).

--- BuildScripts/UnitTest.py
+++ BuildScripts/UnitTest.py
@@ -9,10 +9,24 @@ import subprocess
 Import('envBase')
 
 testEnv = envBase.Clone()
-exePath = os.path.join( testEnv["PACKAGES"], 'NUnit.ConsoleRunner', 'tools', 'nunit3-console.exe' )
+nunitExePath = os.path.join(
+    testEnv["PACKAGES"],
+    'NUnit.ConsoleRunner',
+    'tools',
+    'nunit3-console.exe'
+)
+
+openCoverPath = os.path.join(
+    testEnv['PACKAGES'],
+    'OpenCover',
+    'tools',
+    'OpenCover.Console.exe'
+)
+
+codeCoverageDir = os.path.join(envBase['REPO_ROOT'], 'CodeCoverage')
 
 def RunUnitTest(target, source, env):
-    args = [exePath]
+    args = [nunitExePath]
     targetStr = str(target[0])
     args += ['/result:' + targetStr]
 
@@ -21,7 +35,43 @@ def RunUnitTest(target, source, env):
 
     return subprocess.call(args)
 
+def RunUnitTestWithCoverage(target, source, env):
+    raise Exception("OpenCover doesn't work with protable pdbs yet.  Can't Execute :(")
+    Execute(
+        Delete(
+            glob.glob( os.path.join( codeCoverageDir, '*' ) )
+        )
+    )
+
+    nunitTarget = str(target[0])
+    openCoverTarget = str(target[1])
+
+    args = [openCoverPath]
+    args += ['-register:user']
+    args += ['-filter:+[*]Chaskis*'] # Search for all namespaces with Chaskis.
+    args += ['-returntargetcode'] # Will return whatever nunit returns.
+    
+    searchDirArgs = '-searchdirs:'
+    for s in source:
+        searchDirArgs += os.path.dirname(str(s)) + ';'
+
+    args += [searchDirArgs.strip()]
+    args += ['-target:' + nunitExePath]
+
+    targetArgs = '-targetargs:/result:' + nunitTarget
+    for s in source:
+        targetArgs += ' ' + str(s)
+
+    args += [targetArgs]
+
+    args += ['-output:' + openCoverTarget]
+
+    print (os.getcwd())
+    print (args)
+    return subprocess.call(args)
+
 testEnv.Append(BUILDERS={"UnitTest" : Builder(action=RunUnitTest)})
+testEnv.Append(BUILDERS={"TestCoverage" : Builder(action=RunUnitTestWithCoverage)})
 
 # Get all .csproj files in the Unit Test folder
 csprojs = glob.glob(os.path.join(testEnv['UNIT_TESTS_DIR'], '*', '*.csproj'))
@@ -38,10 +88,20 @@ for csproj in csprojs:
         )
     ]
 
-unitTestTarget = testEnv.UnitTest(
-    target=os.path.join(envBase['REPO_ROOT'], 'TestResult', 'TestResult.xml'),
-    source=sources
-)
+if (envBase['CODE_COVERAGE']):
+    targets = [
+        os.path.join(envBase['REPO_ROOT'], 'TestResult', 'TestResult.xml'),
+        os.path.join(envBase['REPO_ROOT'], 'CodeCoverage', 'coverage.xml')
+    ]
+    unitTestTarget = testEnv.TestCoverage(
+        target = targets,
+        source=sources
+    )
+else:
+    unitTestTarget = testEnv.UnitTest(
+        target=os.path.join(envBase['REPO_ROOT'], 'TestResult', 'TestResult.xml'),
+        source=sources
+    )
 
 AlwaysBuild(unitTestTarget)
 
