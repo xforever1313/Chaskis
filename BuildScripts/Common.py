2b2e893838ab06998cd3e0017e4975c383e1b133
Seth Hendrick <seth@shendrick.net>
2018-07-11 19:50:35 -0400

    Added saved checksum info for chaskis.deb for the most recent release.

--- BuildScripts/Common.py
+++ BuildScripts/Common.py
@@ -1,5 +1,6 @@
 import hashlib
 import io
+import sys
 
 def Checksum(target, source, env):
     with io.open(str(source[0]), 'rb') as inFile:
@@ -8,4 +9,8 @@ def Checksum(target, source, env):
         hash = h.hexdigest()
 
     with io.open(str(target[0]), 'w', encoding='utf-8') as outFile:
-        outFile.write(str(hash))
+        # Need to use unicode in python2, but str in python3.
+        if (sys.version_info.major == 2):
+            outFile.write(unicode(hash))
+        else:
+            outFile.write(str(hash))
