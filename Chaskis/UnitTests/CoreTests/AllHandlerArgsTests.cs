8a27e186646f34f2681fffe81f65c7b4bca15bba
Seth Hendrick <seth@shendrick.net>
2018-09-09 17:36:36 -0400

    Added Unit Tests for existing HandlerArgs classes.

---
+++ Chaskis/UnitTests/CoreTests/AllHandlerArgsTests.cs
@@ -0,0 +1,37 @@
+﻿//
+//          Copyright Seth Hendrick 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using Chaskis.Core;
+using Moq;
+using NUnit.Framework;
+
+namespace Chaskis.UnitTests.CoreTests
+{
+    [TestFixture]
+    public class AllHandlerArgsTests
+    {
+        // ---------------- Tests ----------------
+
+        /// <summary>
+        /// Ensures the properties get set correctly during construction.
+        /// </summary>
+        [Test]
+        public void ConstructorTest()
+        {
+            Mock<IIrcWriter> writer = new Mock<IIrcWriter>( MockBehavior.Strict );
+            const string line = "line";
+
+            AllHandlerArgs uut = new AllHandlerArgs(
+                writer.Object,
+                line
+            );
+
+            Assert.AreSame( writer.Object, uut.Writer );
+            Assert.AreEqual( line, uut.Line );
+        }
+    }
+}
