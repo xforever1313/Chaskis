7f9a5a26986348922e2a2f747f13b330282f6ca2
Seth Hendrick <seth@shendrick.net>
2020-02-15 17:19:42 -0500

    Ported UserListBotTests over to NUnit from FitNesse.

---
+++ Chaskis/RegressionTests/TestFixtures/Tests/PluginTests/UserListBotTests.cs
@@ -0,0 +1,62 @@
+﻿//
+//          Copyright Seth Hendrick 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using Chaskis.RegressionTests.TestCore;
+using NUnit.Framework;
+
+namespace Chaskis.RegressionTests.Tests.PluginTests
+{
+    [TestFixture]
+    public class UserListBotTests
+    {
+        // ---------------- Fields ----------------
+
+        private ChaskisTestFramework testFrame;
+
+        // ---------------- Setup / Teardown ----------------
+
+        [OneTimeSetUp]
+        public void FixtureSetup()
+        {
+            this.testFrame = new ChaskisTestFramework();
+
+            ChaskisFixtureConfig fixtureConfig = new ChaskisFixtureConfig
+            {
+                Environment = "UserListBotEnvironment"
+            };
+
+            this.testFrame.PerformFixtureSetup( fixtureConfig );
+        }
+
+        [OneTimeTearDown]
+        public void FixtureTeardown()
+        {
+            this.testFrame?.PerformFixtureTeardown();
+        }
+
+        public void TestSetup()
+        {
+            this.testFrame.PerformTestSetup();
+        }
+
+        public void TestTeardown()
+        {
+            this.testFrame.PerformTestTeardown();
+        }
+
+        // ---------------- Tests ----------------
+
+        /// <summary>
+        /// Ensures the plugin loads by itself without issue.
+        /// </summary>
+        [Test]
+        public void DoPluginLoadTest()
+        {
+            CommonPluginTests.DoPluginLoadTest( this.testFrame, "userlistbot" );
+        }
+    }
+}
