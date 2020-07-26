43f82edaff523bcd5a914353cce58344dcd6df15
Seth Hendrick <seth@shendrick.net>
2020-07-26 16:46:36 -0400

    Initial commit of MeetBot.
    Right now, just the documentation of the commands to run, and the plugin skeleton.

---
+++ Chaskis/Plugins/MeetBot/Enums.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chaskis.Plugins.MeetBot
+{
+    /// <summary>
+    /// See the Commands.xml file in the Documentation folder
+    /// for what each of these actions represent.
+    /// </summary>
+    /// <remarks>
+    /// Do not rename these or XML parsing will break.
+    /// </remarks>
+    public enum MeetingAction
+    {
+        StartMeeting,
+
+        EndMeeting,
+
+        Topic,
+
+        Agree,
+
+        Chair,
+
+        Unchair,
+
+        Action,
+
+        Info,
+
+        Link,
+
+        Unlink,
+
+        Help,
+
+        Accept,
+
+        Reject,
+
+        Save,
+
+        CancelMeeting,
+
+        Purge,
+
+        Silence,
+
+        Voice,
+
+        Banish
+    }
+
+    /// <summary>
+    /// Who is allowed to execute a command?
+    /// All commands are defaulted to <see cref="CommandRestriction.Anyone"/>
+    /// </summary>
+    /// <remarks>
+    /// Do not rename these or XML parsing will break.
+    /// </remarks>
+    public enum CommandRestriction
+    {
+        /// <summary>
+        /// Anyone can perform a command.
+        /// </summary>
+        Anyone,
+
+        /// <summary>
+        /// Only Chairs and Bot Admins can use this command.
+        /// </summary>
+        ChairsAndBotAdmins,
+
+        /// <summary>
+        /// Only chairs can use the command.
+        /// </summary>
+        ChairsOnly
+    }
+}
---
+++ Chaskis/Plugins/MeetBot/MeetBot.cs
@@ -0,0 +1,105 @@
+﻿//
+//          Copyright Seth Hendrick 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System.Collections.Generic;
+using System.IO;
+using Chaskis.Core;
+using SethCS.Basic;
+
+namespace Chaskis.Plugins.MeetBot
+{
+    [ChaskisPlugin( PluginName )]
+    public class MeetBot : IPlugin
+    {
+        // ---------------- Fields ----------------
+
+        internal const string PluginName = "MeetBot";
+
+        private static readonly string versionStr;
+
+        private GenericLogger logger;
+
+        private string pluginDir;
+
+        private string defaultNotesDirectory;
+
+        private readonly List<IIrcHandler> ircHandlers;
+
+        // ---------------- Constructor ----------------
+
+        public MeetBot()
+        {
+            this.ircHandlers = new List<IIrcHandler>();
+        }
+
+        static MeetBot()
+        {
+            versionStr = typeof( MeetBot ).Assembly.GetName().Version.ToString( 3 );
+        }
+
+        // ---------------- Properties ----------------
+
+        public string SourceCodeLocation
+        {
+            get
+            {
+                return "https://github.com/xforever1313/Chaskis/tree/master/Chaskis/Plugins/MeetBot";
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return versionStr;
+            }
+        }
+
+        public string About
+        {
+            get
+            {
+                return "I help take meeting over IRC";
+            }
+        }
+
+        // ---------------- Functions ----------------
+
+        public void Init( PluginInitor initor )
+        {
+            this.logger = initor.Log;
+
+            this.pluginDir = Path.Combine(
+                initor.ChaskisConfigPluginRoot,
+                PluginName
+            );
+
+            this.defaultNotesDirectory = Path.Combine(
+                this.pluginDir,
+                "meeting_notes"
+             );
+        }
+
+        public void HandleHelp( MessageHandlerArgs msgArgs, string[] helpArgs )
+        {
+            msgArgs.Writer.SendMessage(
+                this.About,
+                msgArgs.Channel
+            );
+        }
+
+        public IList<IIrcHandler> GetHandlers()
+        {
+            return this.ircHandlers.AsReadOnly();
+        }
+
+        public void Dispose()
+        {
+            // Nothing to Dispose.
+        }
+    }
+}
