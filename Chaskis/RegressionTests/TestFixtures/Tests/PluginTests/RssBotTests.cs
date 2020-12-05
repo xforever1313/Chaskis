202557db301a5c7f286ff802078fd464e012834c
Seth Hendrick <seth@shendrick.net>
2020-12-05 15:55:56 -0500

    Issue: #36 Added regression test to ensure the bot doesn't crash if it fails to get an RSS feed.

--- Chaskis/RegressionTests/TestFixtures/Tests/PluginTests/RssBotTests.cs
+++ Chaskis/RegressionTests/TestFixtures/Tests/PluginTests/RssBotTests.cs
@@ -11,166 +11,283 @@ using NUnit.Framework;
 
 namespace Chaskis.RegressionTests.Tests.PluginTests
 {
-    [TestFixture]
+    internal interface IRssBotTestFixture
+    {
+        ChaskisTestFramework TestFrame { get; }
+
+        string TestFilesLocation { get; }
+    }
+
     public class RssBotTests
     {
         // ---------------- Fields ----------------
 
-        private ChaskisTestFramework testFrame;
-
-        private string testFilesLocation;
-
         private const ushort httpServerPort = 32101;
 
         private static readonly string url = $"http://127.0.0.1:{httpServerPort}";
-        private const string xmlFileName = "/feed.xml";
-        private static readonly string feedUrl = $"{url}{xmlFileName}";
+        internal static string XmlFileName = "/feed.xml";
+        internal static readonly string FeedUrl = $"{url}{XmlFileName}";
 
-        // ---------------- Setup / Teardown ----------------
+        // ---------------- Helper Classes ----------------
 
-        [OneTimeSetUp]
-        public void FixtureSetup()
+        [TestFixture]
+        public class HappyPathRssBotTests : IRssBotTestFixture
         {
-            this.testFrame = new ChaskisTestFramework();
-            this.testFrame.SetupLog();
+            // ---------------- Properties ----------------
 
-            ChaskisFixtureConfig fixtureConfig = new ChaskisFixtureConfig
+            public ChaskisTestFramework TestFrame { get; private set; }
+
+            public string TestFilesLocation { get; private set; }
+
+            // ---------------- Setup / Teardown ----------------
+
+            [OneTimeSetUp]
+            public void FixtureSetup()
             {
-                Environment = "rssbotenvironment"
-            };
+                this.TestFrame = new ChaskisTestFramework();
+                this.TestFrame.SetupLog();
 
-            this.testFilesLocation = Path.Combine(
-                this.testFrame.EnvironmentManager.ChaskisProjectRoot,
-                "UnitTests",
-                "PluginTests",
-                "Plugins",
-                "RssBot",
-                "TestFiles"
-            );
+                ChaskisFixtureConfig fixtureConfig = new ChaskisFixtureConfig
+                {
+                    Environment = "rssbotenvironment"
+                };
 
-            this.testFrame.HttpServer.MapFileToUrl(
-                Path.Combine( this.testFilesLocation, "Reddit_Init.xml" ),
-                xmlFileName
-            );
-            this.testFrame.HttpServer.StartHttpServer( httpServerPort );
+                this.TestFilesLocation = Path.Combine(
+                    this.TestFrame.EnvironmentManager.ChaskisProjectRoot,
+                    "UnitTests",
+                    "PluginTests",
+                    "Plugins",
+                    "RssBot",
+                    "TestFiles"
+                );
 
-            // Now, perform the common fixture setup.
-            this.testFrame.PerformFixtureSetup( fixtureConfig );
+                this.TestFrame.HttpServer.MapFileToUrl(
+                    Path.Combine( this.TestFilesLocation, "Reddit_Init.xml" ),
+                    XmlFileName
+                );
+                this.TestFrame.HttpServer.StartHttpServer( httpServerPort );
 
-            this.testFrame.IrcServer.SendMessageToChannelAs(
+                // Now, perform the common fixture setup.
+                this.TestFrame.PerformFixtureSetup( fixtureConfig );
+
+                this.SetVerbosity();
+            }
+
+            [OneTimeTearDown]
+            public void FixtureTeardown()
+            {
+                this.TestFrame?.PerformFixtureTeardown();
+            }
+
+            [SetUp]
+            public void TestSetup()
+            {
+                this.TestFrame.PerformTestSetup();
+                this.TestFrame.HttpServer.SetHttpServerHangTimeTo( 0 );
+            }
+
+            [TearDown]
+            public void TestTeardown()
+            {
+                this.TestFrame.PerformTestTeardown();
+            }
+
+            // ---------------- Tests ----------------
+
+            /// <summary>
+            /// Ensures the plugin loads by itself without issue.
+            /// </summary>
+            [Test]
+            public void DoPluginLoadTest()
+            {
+                CommonPluginTests.DoPluginLoadTest( this.TestFrame, "rssbot" );
+            }
+
+            [Test]
+            public void UpdateTest()
+            {
+                Step.Run(
+                    "Ensure if there are no updates, no messages get sent.",
+                    () =>
+                    {
+                        this.TriggerUpdate();
+                        this.WaitForNoUpdatesMessage();
+                    }
+                );
+
+                // Now do an update.
+                this.DoUpdateTest();
+
+                Step.Run(
+                    "Update feed with less items in it.  Should get no response",
+                    () =>
+                    {
+                        this.TestFrame.HttpServer.MapFileToUrl(
+                            Path.Join( this.TestFilesLocation, "Reddit_Init_Less_Items.xml" ),
+                            XmlFileName
+                        );
+
+                        this.TriggerUpdate();
+                        this.WaitForNoUpdatesMessage();
+                    }
+                );
+            }
+        }
+
+        /// <summary>
+        /// Ensures if our bot gets a 404, we don't crash.
+        /// </summary>
+        [TestFixture]
+        public class RssBot404Tests : IRssBotTestFixture
+        {
+            // ---------------- Properties ----------------
+
+            public ChaskisTestFramework TestFrame { get; private set; }
+
+            public string TestFilesLocation { get; private set; }
+
+            // ---------------- Setup / Teardown ----------------
+
+            [OneTimeSetUp]
+            public void FixtureSetup()
+            {
+                this.TestFrame = new ChaskisTestFramework();
+                this.TestFrame.SetupLog();
+
+                ChaskisFixtureConfig fixtureConfig = new ChaskisFixtureConfig
+                {
+                    Environment = "rssbotenvironment"
+                };
+
+                this.TestFilesLocation = Path.Combine(
+                    this.TestFrame.EnvironmentManager.ChaskisProjectRoot,
+                    "UnitTests",
+                    "PluginTests",
+                    "Plugins",
+                    "RssBot",
+                    "TestFiles"
+                );
+
+                // Defer to the test to start the HTTP server.  What we expect to
+                // happen here is the process to NOT crash if it
+                // gets an exception while trying to get the RSS feed.
+
+                // Now, perform the common fixture setup.
+                this.TestFrame.PerformFixtureSetup( fixtureConfig );
+
+                this.SetVerbosity();
+            }
+
+            [OneTimeTearDown]
+            public void FixtureTeardown()
+            {
+                this.TestFrame?.PerformFixtureTeardown();
+            }
+
+            [SetUp]
+            public void TestSetup()
+            {
+                this.TestFrame.PerformTestSetup();
+                this.TestFrame.HttpServer.SetHttpServerHangTimeTo( 0 );
+            }
+
+            [TearDown]
+            public void TestTeardown()
+            {
+                this.TestFrame.PerformTestTeardown();
+            }
+
+            // ---------------- Tests ----------------
+
+            [Test]
+            public void DoUpdateAfterFailureTest()
+            {
+                this.TestFrame.HttpServer.MapFileToUrl(
+                    Path.Combine( this.TestFilesLocation, "Reddit_Init.xml" ),
+                    XmlFileName
+                );
+                this.TestFrame.HttpServer.StartHttpServer( httpServerPort );
+
+                // We should trigger and ensure nothing goes out.
+                // we don't want to spam anything by getting our first update.
+
+                Step.Run(
+                    "Ensure our first update results in no messages being sent; do not want to spam channels.",
+                    () =>
+                    {
+                        this.TriggerUpdate();
+                        this.WaitForNoUpdatesMessage();
+                    }
+                );
+
+                // Now do an update.
+                this.DoUpdateTest();
+            }
+        }
+    }
+
+    internal static class IRssBotTestFixtureExtensions
+    {
+        public static void SetVerbosity( this IRssBotTestFixture fixture )
+        {
+            fixture.TestFrame.IrcServer.SendMessageToChannelAs(
                 $"!{TestConstants.BotName} debug verbosity rssbot 3",
                 TestConstants.BotName,
                 TestConstants.AdminUserName
             );
 
-            this.testFrame.IrcServer.WaitForMessageOnChannel(
+            fixture.TestFrame.IrcServer.WaitForMessageOnChannel(
                 @"'rssbot'\s+log\s+verbosity\s+has\s+been\s+set\s+to\s+'3'",
                 TestConstants.AdminUserName
             ).FailIfFalse( "Could not turn on RSS Debug" );
         }
 
-        [OneTimeTearDown]
-        public void FixtureTeardown()
+        public static void TriggerUpdate( this IRssBotTestFixture fixture )
         {
-            this.testFrame?.PerformFixtureTeardown();
-        }
-
-        [SetUp]
-        public void TestSetup()
-        {
-            this.testFrame.PerformTestSetup();
-            this.testFrame.HttpServer.SetHttpServerHangTimeTo( 0 );
-        }
-
-        [TearDown]
-        public void TestTeardown()
-        {
-            this.testFrame.PerformTestTeardown();
-        }
-
-        // ---------------- Tests ----------------
-
-        /// <summary>
-        /// Ensures the plugin loads by itself without issue.
-        /// </summary>
-        [Test]
-        public void DoPluginLoadTest()
-        {
-            CommonPluginTests.DoPluginLoadTest( this.testFrame, "rssbot" );
-        }
-
-        [Test]
-        public void UpdateTest()
-        {
-            Step.Run(
-                "Ensure if there are no updates, no messages get sent.",
-                () =>
-                {
-                    this.TriggerUpdate();
-                    this.WaitForNoUpdatesMessage();
-                }
-            );
-
-            Step.Run(
-                "Update feed with more items in it.",
-                () =>
-                {
-                    this.testFrame.HttpServer.MapFileToUrl(
-                        Path.Join( this.testFilesLocation, "Reddit_Update.xml" ),
-                        xmlFileName
-                    );
-
-                    this.TriggerUpdate();
-
-                    this.testFrame.IrcServer.WaitForMessageOnChannel(
-                        @".+Welcome\s+to\s+/r/csharp!\s+Read\s+this\s+post\s+before\s+submitting\..+",
-                        TestConstants.Channel1
-                    ).FailIfFalse( "Did not get feed updates" );
-
-                    this.testFrame.IrcServer.WaitForMessageOnChannel(
-                        @".+PenguinUpload\s+-\s+file\s+hosting\s+service\s+built\s+with\s+ASP\.NET\s+Core,\s+NancyFx,\s+and\s+Vue\.js.+",
-                        TestConstants.Channel1
-                    ).FailIfFalse( "Did not get feed updates" );
-
-                    this.testFrame.IrcServer.WaitForMessageOnChannel(
-                        @".+C#\s+7\s+Series,\s+Part\s+2:\s+Async\s+Main.+",
-                        TestConstants.Channel1
-                    ).FailIfFalse( "Did not get feed updates" );
-                }
-            );
-
-            Step.Run(
-                "Update feed with less items in it.  Should get no response",
-                () =>
-                {
-                    this.testFrame.HttpServer.MapFileToUrl(
-                        Path.Join( this.testFilesLocation, "Reddit_Init_Less_Items.xml" ),
-                        xmlFileName
-                    );
-
-                    this.TriggerUpdate();
-                    this.WaitForNoUpdatesMessage();
-                }
-            );
-        }
-
-        // ---------------- Test Helpers ----------------
-
-        private void TriggerUpdate()
-        {
-            this.testFrame.IrcServer.SendMessageToChannelAs(
-                $"!debug rssbot updatefeed {feedUrl}",
+            fixture.TestFrame.IrcServer.SendMessageToChannelAs(
+                $"!debug rssbot updatefeed {RssBotTests.FeedUrl}",
                 TestConstants.Channel1,
                 TestConstants.AdminUserName
             );
         }
 
-        private void WaitForNoUpdatesMessage()
+        public static void WaitForNoUpdatesMessage( this IRssBotTestFixture fixture )
         {
-            this.testFrame.ProcessRunner.WaitForStringFromChaskis(
+            fixture.TestFrame.ProcessRunner.WaitForStringFromChaskis(
                 @"No\s+updates\s+for\s+RSS\s+feed",
                 10 * 1000
             ).FailIfFalse( "Did not get debug message from process" );
+        }
+
+        public static void DoUpdateTest( this IRssBotTestFixture fixture )
+        {
+            Step.Run(
+                "Update feed with more items in it.",
+                () =>
+                {
+                    fixture.TestFrame.HttpServer.MapFileToUrl(
+                        Path.Join( fixture.TestFilesLocation, "Reddit_Update.xml" ),
+                        RssBotTests.XmlFileName
+                    );
+
+                    fixture.TriggerUpdate();
+
+                    fixture.TestFrame.IrcServer.WaitForMessageOnChannel(
+                        @".+Welcome\s+to\s+/r/csharp!\s+Read\s+this\s+post\s+before\s+submitting\..+",
+                        TestConstants.Channel1
+                    ).FailIfFalse( "Did not get feed updates" );
+
+                    fixture.TestFrame.IrcServer.WaitForMessageOnChannel(
+                        @".+PenguinUpload\s+-\s+file\s+hosting\s+service\s+built\s+with\s+ASP\.NET\s+Core,\s+NancyFx,\s+and\s+Vue\.js.+",
+                        TestConstants.Channel1
+                    ).FailIfFalse( "Did not get feed updates" );
+
+                    fixture.TestFrame.IrcServer.WaitForMessageOnChannel(
+                        @".+C#\s+7\s+Series,\s+Part\s+2:\s+Async\s+Main.+",
+                        TestConstants.Channel1
+                    ).FailIfFalse( "Did not get feed updates" );
+                }
+            );
         }
     }
 }
