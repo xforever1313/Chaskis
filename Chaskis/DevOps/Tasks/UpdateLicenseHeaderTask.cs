be9ed73beaf0cf433d2f3d02debfb2a0c54dc7b7
Seth Hendrick <seth@shendrick.net>
2021-04-25 16:04:21 -0400

    Happy New Year!

--- Chaskis/DevOps/Tasks/UpdateLicenseHeaderTask.cs
+++ Chaskis/DevOps/Tasks/UpdateLicenseHeaderTask.cs
@@ -1,5 +1,5 @@
-﻿//
-//          Copyright Seth Hendrick 2016-2020.
+//
+//          Copyright Seth Hendrick 2016-2021.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
@@ -30,7 +30,7 @@ namespace DevOps.Tasks
 
 ";
         const string oldLicenseRegex1 =
-@"//
+@"^//
 //\s+Copyright\s+Seth\s+Hendrick\s+\d+-?\d*\.?
 //\s+Distributed\s+under\s+the\s+Boost\s+Software\s+License,\s+Version\s+1\.0\.?
 //\s+\(See\s+accompanying\s+file\s+[./]*LICENSE_1_0\.txt\s+or\s+copy\s+at
@@ -38,13 +38,7 @@ namespace DevOps.Tasks
 //[\n\r\s]*";
 
         const string oldLicenseRegex2 =
-@"//\s+Copyright\s+Seth\s+Hendrick\s+\d+-?\d*\.?
-//\s+Distributed\s+under\s+the\s+Boost\s+Software\s+License,\s+Version\s+1\.0\.?
-//\s+\(See\s+accompanying\s+file\s+[./]*LICENSE_1_0\.txt\s+or\s+copy\s+at
-//\s+http://www.boost.org/LICENSE_1_0\.txt\)[\n\r\s]*";
-
-        const string oldLicenseRegex3 =
-@"//
+@"^//
 //\s+Copyright\s+Seth\s+Hendrick\s+[^\n\r]+\.?
 //\s+Distributed\s+under\s+the\s+Boost\s+Software\s+License,\s+Version\s+1\.0\.?
 //\s+\(See\s+accompanying\s+file\s+[./]*LICENSE_1_0\.txt\s+or\s+copy\s+at
@@ -63,7 +57,6 @@ namespace DevOps.Tasks
 
             settings.OldHeaderRegexPatterns.Add( oldLicenseRegex1 );
             settings.OldHeaderRegexPatterns.Add( oldLicenseRegex2 );
-            settings.OldHeaderRegexPatterns.Add( oldLicenseRegex3 );
 
             settings.FileFilter = SolutionProjectHelpers.DefaultCsFileFilter;
 
