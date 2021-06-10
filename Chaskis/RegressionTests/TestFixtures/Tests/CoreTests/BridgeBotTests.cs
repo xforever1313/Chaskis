2824e033da16d43cf0f22ec408667f8806c1607f
Seth Hendrick <seth@shendrick.net>
2021-06-09 22:40:38 -0400

    Added a unit test to ensure we handle STX characters in bridge bots correctly.

---
+++ Chaskis/RegressionTests/TestFixtures/Tests/CoreTests/BridgeBotTests.cs
@@ -0,0 +1,94 @@
+﻿//
+//          Copyright Seth Hendrick 2016-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using Chaskis.RegressionTests.TestCore;
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chaskis.RegressionTests.Tests.CoreTests
+{
+    public class BridgeBotTests
+    {
+        // ---------------- Fields ----------------
+
+        private ChaskisTestFramework testFrame;
+
+        // ---------------- Setup / Teardown ----------------
+
+        [OneTimeSetUp]
+        public void FixtureSetup()
+        {
+            this.testFrame = new ChaskisTestFramework();
+
+            ChaskisFixtureConfig fixtureConfig = new ChaskisFixtureConfig
+            {
+                Environment = "BridgeBotEnvironment"
+            };
+
+            this.testFrame.PerformFixtureSetup( fixtureConfig );
+        }
+
+        [OneTimeTearDown]
+        public void FixtureTeardown()
+        {
+            this.testFrame?.PerformFixtureTeardown();
+        }
+
+        [SetUp]
+        public void TestSetup()
+        {
+            this.testFrame.PerformTestSetup();
+        }
+
+        [TearDown]
+        public void TestTeardown()
+        {
+            this.testFrame.PerformTestTeardown();
+        }
+
+        // ---------------- Tests ----------------
+
+        /// <summary>
+        /// Tests a bug where if a STX character is in the bridge user,
+        /// bridge bots don't work.
+        /// 
+        /// This was found in the #rit-foss channel.
+        /// </summary>
+        [Test]
+        public void BridgeBotWithStxResponse()
+        {
+            Step.Run(
+                "With stx character",
+                () =>
+                {
+                    this.testFrame.IrcServer.SendMessageToChannelAsWaitMsg(
+                        $"<xforever1313> Hello {TestConstants.BotName}",
+                        TestConstants.Channel1,
+                        "tg-ritfoss1",
+                        "Hello xforever1313"
+                    ).FailIfFalse( "Did not get a response." );
+                }
+            );
+
+            Step.Run(
+                "Without stx character",
+                () =>
+                {
+                    this.testFrame.IrcServer.SendMessageToChannelAsWaitMsg(
+                        $"<xforever1313> Hello {TestConstants.BotName}",
+                        TestConstants.Channel1,
+                        "tg-ritfoss1",
+                        "Hello xforever1313"
+                    ).FailIfFalse( "Did not get a response." );
+                }
+            );
+        }
+    }
+}
